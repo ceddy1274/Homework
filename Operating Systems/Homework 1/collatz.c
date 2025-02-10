d798de5c710a56eb7576dae7bd95cd26ba8c8d8a
ceddy1274 <ceddy1274@gmail.com>
2025-02-10 15:12:56 -0600

    HW files

---
+++ Operating Systems/Homework 1/collatz.c
@@ -0,0 +1,7 @@
+/*GOAL: Write a C program to generate Collatz conjecture in a child process*/
+
+/*
+The Collatz conjecture states for any positive integer n,
+   n = n/2, if n is even
+   n = 3n + 1, if n is odd
+*/
---
+++ Operating Systems/Homework 1/zombie.c
@@ -0,0 +1,1 @@
+/*GOAL: Create a C program that forks a child process and ultimately becomes a zombie process (zombie must stay for at least 10 seconds)*/
